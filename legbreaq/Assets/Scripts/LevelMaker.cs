2b8cab4edd7263ec1d2da5484368224bff3cb7e1
cdotson4 <38544442+cdotson4@users.noreply.github.com>
2020-02-05 11:18:10 -0700

    Working Generation V.1
    Able to generate structures with one room prefab repeated.

---
+++ legbreaq/Assets/Scripts/LevelMaker.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMaker : MonoBehaviour
+{
+    enum gridSpace {empty, room};
+    gridSpace[,] grid;
+    int roomHeight, roomWidth;
+    Vector2 roomSizeWorldUnits = new Vector2(100, 100);
+    public float worldUnitsInOneGridCell = 10;
+    struct walker
+    {
+        public Vector2 dir;
+        public Vector2 pos;
+    }
+    List<walker> walkers;
+    public float chanceWalkerChangeDir = 0.5f, chanceWalkerSpawn = 0.05f;
+    public float chanceWalkerDestoy = 0.05f;
+    public int maxWalkers = 10;
+    public float percentToFill = 0.2f;
+    public GameObject roomObj;
+    void Start()
+    {
+        Setup();
+        CreateRooms();
+        SpawnLevel();
+    }
+    void Setup()
+    {
+        //find grid size
+        roomHeight = Mathf.RoundToInt(roomSizeWorldUnits.x / worldUnitsInOneGridCell);
+        roomWidth = Mathf.RoundToInt(roomSizeWorldUnits.y / worldUnitsInOneGridCell);
+        //create grid
+        grid = new gridSpace[roomWidth, roomHeight];
+        //set grid's default state
+        for (int x = 0; x < roomWidth - 1; x++)
+        {
+            for (int y = 0; y < roomHeight - 1; y++)
+            {
+                //make every cell "empty"
+                grid[x, y] = gridSpace.empty;
+            }
+        }
+        //set first walker
+        //init list
+        walkers = new List<walker>();
+        //create a walker 
+        walker newWalker = new walker();
+        newWalker.dir = RandomDirection();
+        //find center of grid
+        Vector2 spawnPos = new Vector2(Mathf.RoundToInt(roomWidth / 2.0f),
+                                        Mathf.RoundToInt(roomHeight / 2.0f));
+        newWalker.pos = spawnPos;
+        //add walker to list
+        walkers.Add(newWalker);
+    }
+    void CreateRooms()
+    {
+        int iterations = 0;//loop will not run forever
+        do
+        {
+            //create room at position of every walker
+            foreach (walker myWalker in walkers)
+            {
+                grid[(int)myWalker.pos.x, (int)myWalker.pos.y] = gridSpace.room;
+            }
+            //chance: destroy walker
+            int numberChecks = walkers.Count; //might modify count while in this loop
+            for (int i = 0; i < numberChecks; i++)
+            {
+                //only if its not the only one, and at a low chance
+                if (Random.value < chanceWalkerDestoy && walkers.Count > 1)
+                {
+                    walkers.RemoveAt(i);
+                    break; //only destroy one per iteration
+                }
+            }
+            //chance: walker pick new direction
+            for (int i = 0; i < walkers.Count; i++)
+            {
+                if (Random.value < chanceWalkerChangeDir)
+                {
+                    walker thisWalker = walkers[i];
+                    thisWalker.dir = RandomDirection();
+                    walkers[i] = thisWalker;
+                }
+            }
+            //chance: spawn new walker
+            numberChecks = walkers.Count; //might modify count while in this loop
+            for (int i = 0; i < numberChecks; i++)
+            {
+                //only if # of walkers < max, and at a low chance
+                if (Random.value < chanceWalkerSpawn && walkers.Count < maxWalkers)
+                {
+                    //create a walker 
+                    walker newWalker = new walker();
+                    newWalker.dir = RandomDirection();
+                    newWalker.pos = walkers[i].pos;
+                    walkers.Add(newWalker);
+                }
+            }
+            //move walkers
+            for (int i = 0; i < walkers.Count; i++)
+            {
+                walker thisWalker = walkers[i];
+                thisWalker.pos += thisWalker.dir;
+                walkers[i] = thisWalker;
+            }
+            //avoid boarder of grid
+            for (int i = 0; i < walkers.Count; i++)
+            {
+                walker thisWalker = walkers[i];
+                //clamp x,y to leave a 1 space boarder: leave room for walls
+                thisWalker.pos.x = Mathf.Clamp(thisWalker.pos.x, 1, roomWidth - 2);
+                thisWalker.pos.y = Mathf.Clamp(thisWalker.pos.y, 1, roomHeight - 2);
+                walkers[i] = thisWalker;
+            }
+            //check to exit loop
+            if ((float)NumberOfRooms() / (float)grid.Length > percentToFill)
+            {
+                break;
+            }
+            iterations++;
+        } while (iterations < 100000);
+    }
+   
+    void SpawnLevel()
+    {
+        for (int x = 0; x < roomWidth; x++)
+        {
+            for (int y = 0; y < roomHeight; y++)
+            {
+                switch (grid[x, y])
+                {
+                    case gridSpace.empty:
+                        break;
+                    case gridSpace.room:
+                        Spawn(x, y, roomObj);
+                        break;
+                }
+            }
+        }
+    }
+    Vector2 RandomDirection()
+    {
+        //pick random int between 0 and 3
+        int choice = Mathf.FloorToInt(Random.value * 3.99f);
+        //use that int to chose a direction
+        switch (choice)
+        {
+            case 0:
+                return Vector2.down;
+            case 1:
+                return Vector2.left;
+            case 2:
+                return Vector2.up;
+            default:
+                return Vector2.right;
+        }
+    }
+    int NumberOfRooms()
+    {
+        int count = 0;
+        foreach (gridSpace space in grid)
+        {
+            if (space == gridSpace.room)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    void Spawn(float x, float y, GameObject toSpawn)
+    {
+        //find the position to spawn
+        Vector2 offset = roomSizeWorldUnits / 2.0f;
+        Vector2 spawnPos = new Vector2(x, y) * worldUnitsInOneGridCell - offset;
+        //spawn object
+        Instantiate(toSpawn, spawnPos, Quaternion.identity);
+    }
+}
---
+++ legbreaq/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    /*
+     * 1: bottom door
+     * 2: top door
+     * 3: left door
+     * 4: right door
+     */
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 2.01f);
+    }
+
+
+
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 2)
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 1)
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
